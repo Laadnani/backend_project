b7a5376aa55099488c00ede9b371ae6eba4deacb
Laadnani <laadnani.med@gmail.com>
2023-06-13 16:49:17 +0100

    commit on - 16:48 the wed 13/6

---
+++ modules/routes.js
@@ -0,0 +1,10 @@
+const {Router} = require ('express');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('using API router')
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require ('express');
+const   route = require('./modules/routes.js');
 
 const app = express ();
 
 const PORT = 5050;
 
-app.listen(PORT, () => console.log('server luanched and listening on port' + PORT));
+app.get('/', (req, res, next) => {
+    res.send('Home Page')
+});
+
+app.use('/api/v1/routes', route );
+
+app.listen(PORT, () => console.log('server lunched and listening on port' + PORT));

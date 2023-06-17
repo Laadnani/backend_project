84606d13e6d335ac322644e49bfa0ea4ec008225
Laadnani <laadnani.med@gmail.com>
2023-06-17 18:58:32 +0200

    routes and route logic entred

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require ('express');
 const router = require('./modules/routes.js');
 
 
-
 const app = express ();
 const PORT = 5050;
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Home Page')
+app.use(express.static('Pages/loginPage'));
+
+
+app.get('/', (req, res) => {
+    res.redirect('/login.html')
+
 });
 
 app.use('/api/v1', router );
